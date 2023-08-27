121c3f451c1123eca2f2f5efc8e32f940c40c503
Phot Koseekrainiramon <z5387411@ad.unsw.edu.au>
2023-08-27 15:50:15 +1000

    counter with redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {Provider} from 'react-redux'
+import myReducers from './reducers'
+import {legacy_createStore as createStore} from 'redux';;
+//Create the store
+const myStore = createStore(myReducers);
+//This will console log the current state everytime the state changes
+myStore.subscribe(()=>console.log(myStore.getState()));
+//Enveloping the App inside the Provider, ensures that the states in the store are available
+//throughout the application
+ReactDOM.render(<Provider store={myStore}><App/></Provider>, document.getElementById('root'));
